628a0d6031d59a5d4e7624307b2dabe567ad15e3
bkandh30 <kandhari.bhavya20@gmail.com>
2025-06-12 17:54:08 -0700

    added view and create route requests

--- main.go
+++ main.go
@@ -9,12 +9,20 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Gistbox"))
 }
 
+func gistView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific gist"))
+}
+
+func gistCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form to create a new gist"))
+}
+
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("/gist/view", gistView)
+	mux.HandleFunc("/gist/create", gistCreate)
 	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	if err != nil {
-		log.Fatal(err)
-	}
+	err := http.ListenAndServe(":4000", nil)
+	log.Fatal(err)
 }
