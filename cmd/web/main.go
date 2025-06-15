1fa41e37b20e748cddcc1eef0e3846c98592d02b
bkandh30 <kandhari.bhavya20@gmail.com>
2025-06-14 20:28:41 -0700

    added dependency injection for custom logger

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,11 +7,19 @@ import (
 	"os"
 )
 
+type application struct {
+	logger *slog.Logger
+}
+
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{AddSource: true}))
+
+	app := &application{
+		logger: logger,
+	}
 
 	mux := http.NewServeMux()
 
@@ -19,10 +27,10 @@ func main() {
 
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("GET /{$}", home)
-	mux.HandleFunc("GET /gist/view/{id}", gistView)
-	mux.HandleFunc("GET /gist/create", gistCreate)
-	mux.HandleFunc("POST /gist/create", gistCreatePost)
+	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("GET /gist/view/{id}", app.gistView)
+	mux.HandleFunc("GET /gist/create", app.gistCreate)
+	mux.HandleFunc("POST /gist/create", app.gistCreatePost)
 
 	logger.Info("Starting server", "addr", *addr)
 	err := http.ListenAndServe(*addr, mux)
